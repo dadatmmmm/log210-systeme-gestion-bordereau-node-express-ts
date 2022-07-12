c9720c749391317b71fe97269573728d17dd3009
Davina <nmbiya1@gmail.com>
2022-07-12 08:51:34 -0400

    Revert "jwt works"
    This reverts commit f1d8e3a0a201277bb3f5db33e8b65fb4bbf36148.

--- src/model/Teacher.ts
+++ src/model/Teacher.ts
@@ -1,30 +1,16 @@
 import md5 = require('md5');
-import  { TeacherJSON, DecodeResult } from '.';
-import { encodeSession } from '../jwt/Encode';
-import { decodeSession } from '../jwt/Decode';
-import { SECRET } from '../jwt/VeriyToken';
+import  { TeacherJSON } from '.';
 
 export class Teacher  {
 
   static login(email: string, password: string) {
     const teachers: TeacherJSON[] = require('../data/teachers.json');
     const teacher = teachers.find(teacher => email == teacher.id);
-    const session = encodeSession(SECRET, {
-      id: email,
-      date: Date.now()
-    });
-    return teacher ? {  ...session, user: teacher } : null;
+    return teacher ? { token: md5(email), user: teacher } : null;
   }
 
   static fromToken(token: string):TeacherJSON {
-
-    const decodeResults = decodeSession(SECRET, token);
-
-    if (!decodeResults?.session) {
-      throw new Error("Decoding did not work");
-    }
-    
-    const teacher = Teacher.all().find(teacher => teacher.id == decodeResults.id);
+    const teacher = Teacher.all().find(teacher => md5(teacher.id) == token);
 
     if (!teacher) {
       throw new Error("Teacher token not found");
